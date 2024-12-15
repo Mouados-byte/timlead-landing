ed0701e75d454101e59fce979e480b52974cf412
Mouados-Byte <imam6mouad@gmail.com>
2024-12-15 18:56:51 +0100

    Work on Seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,19 +28,42 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        {/* Preconnect for fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link rel="icon" type="image/png" href="/favicon.png" />
-        {/* <link rel="alternate" type="application/rss+xml" href={EnvVars.URL + 'rss'} title="RSS 2.0" /> */}
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-          ga('create', 'UA-117119829-1', 'auto');
-          ga('send', 'pageview');`,
-          }}
-        /> */}
-        {/* <script async src="https://www.google-analytics.com/analytics.js"></script> */}
+
+        {/* Page Title */}
+        <meta name="description" content="TIMLEAD - Solution de gestion des interventions techniques et suivi des techniciens en temps réel" />
+
+        {/* OpenGraph Meta Tags (for Facebook, LinkedIn, WhatsApp) */}
+        <meta property="og:title" content="TIMLEAD - Gestion des interventions techniques" />
+        <meta property="og:description" content="Optimisez vos opérations techniques avec TIMLEAD : suivi GPS, gestion des tickets, planification des interventions" />
+        <meta property="og:image" content="/opengraph.png" />
+        <meta property="og:image:alt" content="Interface TIMLEAD de gestion des interventions" />
+        <meta property="og:url" content="https://getstarted.timlead.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="TIMLEAD" />
+
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="TIMLEAD - Gestion des interventions techniques" />
+        <meta name="twitter:description" content="Optimisez vos opérations techniques avec TIMLEAD : suivi GPS, gestion des tickets, planification des interventions" />
+        <meta name="twitter:image" content="/twitter.png" />
+        <meta name="twitter:image:alt" content="Interface TIMLEAD de gestion des interventions" />
+        <meta name="twitter:site" content="@timlead" />
+
+        {/* Robots and Canonical */}
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://getstarted.timlead.com" />
+
+        {/* Keywords */}
+        <meta name="keywords" content="gestion interventions, suivi GPS techniciens, maintenance technique, GMAO, gestion tickets, planification interventions" />
+
+        {/* Viewport for Responsiveness */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+
       <ColorModeScript />
       <GlobalStyle />
 
