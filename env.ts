014eeee32b029bb318479cd9a981e9f34aad27e3
Mouados-Byte <imam6mouad@gmail.com>
2025-04-13 18:02:50 +0100

    Change title and description

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
 export const EnvVars = {
-  SITE_NAME: 'TimLead | Telecominder',
+  SITE_NAME: 'Timlead – Operations, Projects, Ticketing, GIS & Chat',
   OG_IMAGES_URL: 'https://tim-lead-ashy.vercel.app/og-images/',
   URL: 'https://tim-lead-ashy.vercel.app/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
